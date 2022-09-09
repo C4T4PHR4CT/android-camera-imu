da9ba81e7738fa29d4575be7dfa883c5bee9ce8c
ln-tmr <89187896+ln-tmr@users.noreply.github.com>
2022-09-09 23:24:22 +0200

    simple write privileges

--- app/src/main/java/de/markusfisch/android/cameraviewdemo/activity/MainActivity.java
+++ app/src/main/java/de/markusfisch/android/cameraviewdemo/activity/MainActivity.java
@@ -1,38 +1,46 @@
 package de.markusfisch.android.cameraviewdemo.activity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import de.markusfisch.android.cameraview.widget.CameraView;
 import de.markusfisch.android.cameraviewdemo.R;
 
 public class MainActivity extends Activity {
-	private static final int REQUEST_CAMERA = 1;
+	private final String[] permissions = new String[] {android.Manifest.permission.CAMERA,android.Manifest.permission.WRITE_EXTERNAL_STORAGE};
+	private int iterator = 0;
 
 	private static boolean frontFacing = false;
 
 	private CameraView cameraView;
 
 	@Override
-	public void onRequestPermissionsResult(
-			int requestCode,
-			String[] permissions,
-			int[] grantResults) {
-		if (requestCode == REQUEST_CAMERA &&
-				grantResults.length > 0 &&
-				grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-			Toast.makeText(
-					this,
-					R.string.error_camera,
-					Toast.LENGTH_SHORT).show();
+	public void onRequestPermissionsResult(int requestCode, String[] _, int[] grantResults) {
+		if (requestCode == iterator && grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+			Toast.makeText(this, R.string.error_camera, Toast.LENGTH_SHORT).show();
 			finish();
-		}
+		} else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+			while (iterator < permissions.length)
+				if (checkSelfPermission(permissions[iterator]) != PackageManager.PERMISSION_GRANTED) {
+					requestPermissions(new String[]{permissions[iterator]}, iterator);
+					break;
+				} else
+					iterator++;
 	}
 
 	@Override
@@ -46,7 +54,7 @@ public class MainActivity extends Activity {
 
 		cameraView = new CameraView(this);
 		cameraView.setUseOrientationListener(true);
-		cameraView.setOnClickListener(v -> invertCamera());
+		cameraView.setOnClickListener(v -> {writeFile("cica");});
 
 		setContentView(cameraView);
 	}
@@ -63,14 +71,30 @@ public class MainActivity extends Activity {
 		closeCameraView();
 	}
 
-	private void checkPermissions() {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-			String permission = android.Manifest.permission.CAMERA;
-			if (checkSelfPermission(permission) !=
-					PackageManager.PERMISSION_GRANTED) {
-				requestPermissions(new String[]{permission}, REQUEST_CAMERA);
-			}
+	private void writeFile(String value){
+		@SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+		File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES)+"/"+timeStamp+".txt");
+		Toast.makeText(this, getExternalFilesDir(Environment.DIRECTORY_PICTURES).toString(), Toast.LENGTH_LONG).show();
+		try {
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(value);
+			bw.close();
 		}
+		catch (IOException e){
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+
+	private void checkPermissions() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+			while (iterator < permissions.length)
+				if (checkSelfPermission(permissions[iterator]) != PackageManager.PERMISSION_GRANTED) {
+					requestPermissions(new String[]{permissions[iterator]}, iterator);
+					break;
+				} else
+					iterator++;
 	}
 
 	private void openCameraView() {
@@ -88,8 +112,6 @@ public class MainActivity extends Activity {
 	}
 
 	private int getFacing() {
-		return frontFacing ?
-				Camera.CameraInfo.CAMERA_FACING_FRONT :
-				Camera.CameraInfo.CAMERA_FACING_BACK;
+		return frontFacing ? Camera.CameraInfo.CAMERA_FACING_FRONT : Camera.CameraInfo.CAMERA_FACING_BACK;
 	}
 }
